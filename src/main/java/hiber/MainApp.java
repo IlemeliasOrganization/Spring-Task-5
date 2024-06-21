a24d5070358115d432b849c0c4f65cdc421e77b2
Ilemelias <ilemelias@icloud.com>
2024-06-21 15:40:15 +0300

    Задача решена

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,17 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+      //Создаем Юзеров с Машинами
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("BMW", 5)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Mercedes", 200)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Audi", 8)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Toyota", 1)));
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +34,13 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car Model = " +user.getCar().getModel());
+         System.out.println("Car Series = " +user.getCar().getSeries());
          System.out.println();
       }
+
+      User user = userService.getUserByCar("BMW", 5);
+      System.out.println("User with BMW 5 series: " + user.getFirstName() + " " + user.getLastName());
 
       context.close();
    }
